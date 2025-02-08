73030a0ab2197625b6fbea8b4a78ef796df7b683
AmanKadam-16 <aman16112002kadam@gmail.com>
2025-02-08 13:26:20 +0530

    build v/19

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"fb0b59d9438bc2950c4afa3af07cc2f82b16db
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4180547105"
+    serviceWorkerVersion: "3887510373"
   }
 });
