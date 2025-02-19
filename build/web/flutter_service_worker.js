915d4186ee9d26256a5f717de1f8c765a30df222
AmanKadam-16 <aman16112002kadam@gmail.com>
2025-02-19 18:54:40 +0530

    build v/23

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5e2a829b56d4720c79858dddfb6a4851
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "558c9d1b5e68b5453de0ca47848ea9d3",
+"flutter_bootstrap.js": "573f761414f981a5ab4ece6cc97ec32e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
